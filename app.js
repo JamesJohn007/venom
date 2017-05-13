0f3b50156cdb7517434283f41751a588e24d95dd
JamesJohn007 <jj465890@gmail.com>
2017-05-13 14:14:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express')
-var app = express()
-var port = process.env.PORT || 8000;
+var express = require('express');
+var app = express();
 
-app.use(express.static('public'));
-app.get('/', function (req, res) {
-  res.sendfile(__dirname+'/public/index.html');
-})
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(port, function(){
-	console.log("server started");
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
